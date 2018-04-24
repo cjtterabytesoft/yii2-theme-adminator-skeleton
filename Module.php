ee65c70ebd2353c9c9f6c294a9fa3e8efb1c0682
cjtterabyte <cjtterabytellc@gmail.com>
2018-04-24 18:24:29 -0400

    Add Module Extension.

---
+++ Module.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+* This file is part of the CJTTERABYTESOFT yii2-theme-adminator-skeleton
+*
+* (c) CJT TERABYTE LLC yii2-theme-adminator-skeleton
+* For the full copyright and license information, please view the LICENSE.md
+* file that was distributed with this source code
+*
+*          @link: https://github.com/cjtterabytesoft/yii2-theme-adminator-skeleton
+*        @author: Wilmer Arámbula <cjtterabytellc@gmail.com>
+*     @copyright: (c) CJT TERABYTE LLC
+*         @theme: [yii2-theme-adminator-skeleton]
+* @Configuration: [Module]
+*         @since: 1.0
+*/
+
+namespace cjtterabytesoft\theme\adminskeleton;
+
+use yii;
+use yii\base\Module as BaseModule;
+
+class Module extends BaseModule
+{
+    const VERSION = '0.0.1-dev';
+    public $defaultRoute = 'pages';
+}
