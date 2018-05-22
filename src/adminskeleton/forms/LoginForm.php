a3b4c90665db4138d59071b349f2ab08b2b6c35c
cjtterabyte <cjtterabytellc@gmail.com>
2018-05-21 20:42:46 -0400

    Fix yii 2.1.

---
+++ src/adminskeleton/forms/LoginForm.php
@@ -0,0 +1,99 @@
+<?php
+
+/**
+* This file is part of the CJTTERABYTESOFT yii2-theme-adminator-skeleton
+*
+* (c) CJT TERABYTE LLC yii2-theme-adminator-skeleton
+* For the full copyright and license information, please view the LICENSE.md
+* file that was distributed with this source code
+*
+*      @link: https://github.com/cjtterabytesoft/yii2-theme-adminator-skeleton
+*    @author: Wilmer Arámbula <cjtterabytellc@gmail.com>
+* @copyright: (c) CJT TERABYTE LLC
+*     @theme: [yii2-theme-adminator-skeleton]
+*     @forms: models[LoginForm]
+*     @since: 1.0
+**/
+
+namespace cjtterabytesoft\theme\adminskeleton\forms;
+
+use Yii;
+use yii\base\Model;
+
+/**
+ * LoginForm is the model behind the login form.
+ *
+ * @property User|null $user This property is read-only.
+ *
+ */
+class LoginForm extends Model
+{
+	public $username;
+	public $password;
+	public $rememberMe = true;
+	private $_user = false;
+	public $verifyCode;
+	
+	/**
+	 * @return array the validation rules.
+	 */
+	
+	public function rules()
+	{
+		return [
+			// username and password are both required
+			[['username', 'password'], 'required'],
+			// rememberMe must be a boolean value
+			['rememberMe', 'boolean'],
+			// password is validated by validatePassword()
+			['password', 'validatePassword'],
+			// verifyCode needs to be entered correctly
+			['verifyCode', 'captcha'],
+		];
+	}
+	
+	/**
+	 * Validates the password.
+	 * This method serves as the inline validation for password.
+	 *
+	 * @param string $attribute the attribute currently being validated
+	 * @param array $params the additional name-value pairs given in the rule
+	 */
+	
+	public function validatePassword($attribute, $params)
+	{
+		if (!$this->hasErrors()) {
+			$user = $this->getUser();
+			if (!$user || !$user->validatePassword($this->password)) {
+				$this->addError($attribute, 'Incorrect username or password.');
+			}
+		}
+	}
+	
+	/**
+	 * Logs in a user using the provided username and password.
+	 * @return bool whether the user is logged in successfully
+	 */
+	
+	public function login()
+	{
+		if ($this->validate()) {
+			return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600*24*30 : 0);
+		}
+		return false;
+	}
+	
+	/**
+	 * Finds user by [[username]]
+	 *
+	 * @return User|null
+	 */
+	
+	public function getUser()
+	{
+		if ($this->_user === false) {
+			$this->_user = User::findByUsername($this->username);
+		}
+		return $this->_user;
+	}
+}
