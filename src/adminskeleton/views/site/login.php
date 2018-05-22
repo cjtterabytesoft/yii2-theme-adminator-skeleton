0d1b9edf236a3f666c4cf970ceeeba0856b030be
cjtterabyte <cjtterabytellc@gmail.com>
2018-05-21 20:09:40 -0400

    Fix Class Yii 2.1.

--- src/adminskeleton/views/site/login.php
+++ src/adminskeleton/views/site/login.php
@@ -26,31 +26,31 @@ $this->params['breadcrumbs_theme'][] = $this->title;
 ?>
 
 <?= Html::beginTag('div', ['class' => 'peers ai-s fxw-nw h-100vh']) ?>
-    <?= Html::beginTag('div', ['class' => 'd-n@sm- peer peer-greed h-100 pos-r bgr-n bgpX-c bgpY-c bgsz-cv',
-        'style' => 'background-image: url("/images/logos/bg.jpg']) ?>
-        <?= Html::beginTag('div', ['class' => 'pos-a centerXY']) ?>
-            <?= Html::beginTag('div', ['class' => 'bgc-white bdrs-50p pos-r', 
-                'style' => 'width: 120px; height: 120px;']) ?>
-                <?= Html::img('/images/logos/logo.png', $options = ['class' => 'pos-a centerXY', 'alt' => '']) ?>
-            <?= Html::endTag('div') ?>
-        <?= Html::endTag('div') ?>
-    <?= Html::endTag('div') ?>
-    <?= Html::beginTag('div', ['class' => 'col-12 col-md-4 peer pX-40 pY-80 h-100 bgc-white scrollable pos-r',
-        'style' => 'min-width: 320px;']) ?>    
-        <?= Breadcrumbs::widget(['links' => isset($this->params['breadcrumbs_theme']) ?
-            $this->params['breadcrumbs_theme'] : []]) ?>         
-        <?= Html::tag('h4', 'Login', ['class' => 'fw-300 c-grey-900 mB-40']) ?>
-        <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-            <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-            <?= $form->field($model, 'password')->passwordInput() ?>
-            <?= $form->field($model, 'rememberMe')->checkbox() ?>
-            <?= $form->field($model, 'reCaptcha')->widget(\himiklab\yii2\recaptcha\ReCaptcha::className()) ?>
-            <?= Html::beginTag('div', ['style' => 'color:#999;margin:1em 0']) ?>
-                If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
-            <?= Html::endTag('div') ?>
-            <?= Html::beginTag('div', ['class' => 'form-group']) ?>
-                <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-            <?= Html::endTag('div') ?>
-        <?php ActiveForm::end(); ?>
-    <?= Html::endTag('div') ?>
+	<?= Html::beginTag('div', ['class' => 'd-n@sm- peer peer-greed h-100 pos-r bgr-n bgpX-c bgpY-c bgsz-cv',
+		'style' => 'background-image: url("/images/logos/bg.jpg']) ?>
+		<?= Html::beginTag('div', ['class' => 'pos-a centerXY']) ?>
+			<?= Html::beginTag('div', ['class' => 'bgc-white bdrs-50p pos-r', 
+				'style' => 'width: 120px; height: 120px;']) ?>
+				<?= Html::img('/images/logos/logo.png', $options = ['class' => 'pos-a centerXY', 'alt' => '']) ?>
+			<?= Html::endTag('div') ?>
+		<?= Html::endTag('div') ?>
+	<?= Html::endTag('div') ?>
+	<?= Html::beginTag('div', ['class' => 'col-12 col-md-4 peer pX-40 pY-80 h-100 bgc-white scrollable pos-r',
+		'style' => 'min-width: 320px;']) ?>    
+		<?= Breadcrumbs::widget(['links' => isset($this->params['breadcrumbs_theme']) ?
+			$this->params['breadcrumbs_theme'] : []]) ?>         
+		<?= Html::tag('h4', 'Login', ['class' => 'fw-300 c-grey-900 mB-40']) ?>
+		<?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+			<?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+			<?= $form->field($model, 'password')->passwordInput() ?>
+			<?= $form->field($model, 'rememberMe')->checkbox() ?>
+			<?= $form->field($model, 'reCaptcha')->widget(\himiklab\yii2\recaptcha\ReCaptcha::class) ?>
+			<?= Html::beginTag('div', ['style' => 'color:#999;margin:1em 0']) ?>
+				If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
+			<?= Html::endTag('div') ?>
+			<?= Html::beginTag('div', ['class' => 'form-group']) ?>
+				<?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+			<?= Html::endTag('div') ?>
+		<?php ActiveForm::end(); ?>
+	<?= Html::endTag('div') ?>
 <?= Html::endTag('div') ?>
--- src/adminskeleton/views/site/signup.php
+++ src/adminskeleton/views/site/signup.php
@@ -26,28 +26,28 @@ $this->params['breadcrumbs_theme'][] = $this->title;
 ?>
 
 <?= Html::beginTag('div', ['class' => 'peers ai-s fxw-nw h-100vh']) ?>
-    <?= Html::beginTag('div', ['class' => 'peer peer-greed h-100 pos-r bgr-n bgpX-c bgpY-c bgsz-cv',
-        'style' => 'background-image: url("/images/logos/bg.jpg']) ?>
-        <?= Html::beginTag('div', ['class' => 'pos-a centerXY']) ?>
-            <?= Html::beginTag('div', ['class' => 'bgc-white bdrs-50p pos-r', 
-                'style' => 'width: 120px; height: 120px;']) ?>
-                <?= Html::img('/images/logos/logo.png', $options = ['class' => 'pos-a centerXY', 'alt' => '']) ?>
-            <?= Html::endTag('div') ?>
-        <?= Html::endTag('div') ?>
-    <?= Html::endTag('div') ?>
-    <?= Html::beginTag('div', ['class' => 'col-12 col-md-4 peer pX-40 pY-80 h-100 bgc-white scrollable pos-r',
-        'style' => 'min-width: 320px;']) ?> 
-        <?= Breadcrumbs::widget(['links' => isset($this->params['breadcrumbs_theme']) ?
-            $this->params['breadcrumbs_theme'] : []]) ?>           
-        <?= Html::tag('h4', 'Register', ['class' => 'fw-300 c-grey-900 mB-40']) ?>
-        <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
-            <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-            <?= $form->field($model, 'email') ?>
-            <?= $form->field($model, 'password')->passwordInput() ?>
+	<?= Html::beginTag('div', ['class' => 'peer peer-greed h-100 pos-r bgr-n bgpX-c bgpY-c bgsz-cv',
+		'style' => 'background-image: url("/images/logos/bg.jpg']) ?>
+		<?= Html::beginTag('div', ['class' => 'pos-a centerXY']) ?>
+			<?= Html::beginTag('div', ['class' => 'bgc-white bdrs-50p pos-r', 
+				'style' => 'width: 120px; height: 120px;']) ?>
+				<?= Html::img('/images/logos/logo.png', $options = ['class' => 'pos-a centerXY', 'alt' => '']) ?>
+			<?= Html::endTag('div') ?>
+		<?= Html::endTag('div') ?>
+	<?= Html::endTag('div') ?>
+	<?= Html::beginTag('div', ['class' => 'col-12 col-md-4 peer pX-40 pY-80 h-100 bgc-white scrollable pos-r',
+		'style' => 'min-width: 320px;']) ?> 
+		<?= Breadcrumbs::widget(['links' => isset($this->params['breadcrumbs_theme']) ?
+			$this->params['breadcrumbs_theme'] : []]) ?>           
+		<?= Html::tag('h4', 'Register', ['class' => 'fw-300 c-grey-900 mB-40']) ?>
+		<?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
+			<?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+			<?= $form->field($model, 'email') ?>
+			<?= $form->field($model, 'password')->passwordInput() ?>
 			<?= $form->field($model, 'reCaptcha')->widget(\himiklab\yii2\recaptcha\ReCaptcha::className()) ?>        
-            <?= Html::beginTag('div', ['class' => 'form-group']) ?>
-                <?= Html::submitButton('Signup', ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
-            <?= Html::endTag('div') ?>
-        <?php ActiveForm::end(); ?>
-    <?= Html::endTag('div') ?>
+			<?= Html::beginTag('div', ['class' => 'form-group']) ?>
+				<?= Html::submitButton('Signup', ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
+			<?= Html::endTag('div') ?>
+		<?php ActiveForm::end(); ?>
+	<?= Html::endTag('div') ?>
 <?= Html::endTag('div') ?>
